58688385452b3aef5523bf81571911488b137719
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2022-11-04 18:45:07 +0900

    [Silver III] Title: 터렛, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    d = ((x1-x2)**2+(y1-y2)**2)**0.5
+    rs = r1+r2
+    rm = abs(r1-r2)
+    if d==0:
+        if r1==r2:
+            print(-1)
+        else:
+            print(0)
+    else:
+        if d==rs or d==rm:
+            print(1)
+        elif d<rs and d>rm:
+            print(2)
+        else:
+            print(0)
