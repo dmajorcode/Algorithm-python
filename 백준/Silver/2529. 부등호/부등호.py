4ff412782f93f0027d5976391b4ae61eda29cb13
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2022-11-27 23:28:58 +0900

    [Silver I] Title: 부등호, Time: 176 ms, Memory: 116564 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -28,10 +28,12 @@ def solve(index, s):
         
     for i in range(10):
         if visited[i]==0:
+            # s[-1]인 이유는 아래의 재귀에서 s에 str을 계속 더해 나가기 때문이다.
             if index==0 or check(s[-1],str(i),signs[index-1]):
                 visited[i] = True
                 solve(index+1,s+str(i))
                 visited[i] = False
+    
     
 solve(0,"")
 print(maxArr)
